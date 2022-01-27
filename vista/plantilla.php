4a567a627422636b243b3b3a3326252ad2efa324
Allan Mendoza <allanmaleman@gmail.com>
2022-01-26 19:15:28 -0600

    Se ajusto la pagina de inicio front-end

--- vista/plantilla.php
+++ vista/plantilla.php
@@ -1,17 +1,21 @@
 <?php
-    //header
-    include_once 'modulos/encabezado.php';
-    include_once 'modulos/menuAndLogo.php';
+    //Header with link
+    include_once 'modulos/encabezadoEtiqueta.php';
 
-    //incluimos el contenido de la pagina
-    if (isset($_GET["ruta"])) {
-        if ($_GET["ruta"] == "index" || $_GET["ruta"] == "proyect" ) {
-            include 'modulos/'.$_GET["ruta"].".php";    
-        }
-    }else {
-        include 'modulos/index.php';
-    }
+    //Body
+    include_once 'modulos/header.php';
+    include_once 'modulos/sliders.php';
+    include_once 'modulos/services.php';
+    include_once 'modulos/welcome.php';
+    include_once 'modulos/offers.php';
+    include_once 'modulos/testimonial.php';
+    include_once 'modulos/business.php';
+    include_once 'modulos/experts.php';
+    include_once 'modulos/emergencyContact.php';
 
-    //Footer
-    include_once 'modulos/pie.php';
+    //Footer 
+    include_once 'modulos/footer.php';
+    include_once 'modulos/formFooter.php';
+    //Javascript Footer
+    include_once 'modulos/footerJsEnd.php';
 ?>
